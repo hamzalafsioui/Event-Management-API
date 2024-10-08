9e5b0d69c26771c7e92eebad8e5a0ef3549d86de
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-08 10:34:10 +0000

    GetUserPaginatedListQuery & GetUserPaginatedListResponse

---
+++ EventManagement.Core/Features/Users/Queries/Models/GetUserPaginatedListQuery.cs
@@ -0,0 +1,14 @@
+﻿using EventManagement.Core.Features.Users.Queries.Results;
+using EventManagement.Core.Wrappers;
+using MediatR;
+
+namespace EventManagement.Core.Features.Users.Queries.Models
+{
+	public class GetUserPaginatedListQuery :IRequest<PaginatedResult<GetUserPaginatedListResponse>>
+	{
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string []? OrderBy { get; set; }
+        public string? Search { get; set; }
+    }
+}
---
+++ EventManagement.Core/Features/Users/Queries/Results/GetUserPaginatedListResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace EventManagement.Core.Features.Users.Queries.Results
+{
+	public class GetUserPaginatedListResponse
+	{
+		public int UserId { get; set; }
+		public required string Username { get; set; }
+		public required string FirstName { get; set; }
+		public required string LastName { get; set; }
+		public required string Email { get; set; }
+		public string? Image { get; set; }
+		public string Role { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
