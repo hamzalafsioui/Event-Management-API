a5930a420cef25a15f878590e540bb3624ebdcf1
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-16 19:50:51 +0000

    Event Controller

---
+++ EventManagement.API/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using EventManagement.API.Base;
+using EventManagement.Core.Features.Events.Queries.Models;
+using EventManagement.Data.AppMetaData;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagement.API.Controllers
+{
+	//[Route("api/[controller]")]
+	[ApiController]
+	public class EventController : AppControllerBase
+	{
+		#region  Functions
+		[HttpGet(Router.EventRouting.GetById)]
+		public async Task<IActionResult> GetEventById([FromRoute] int id)
+		{
+			var response = await Mediator.Send(new GetEventByIdQuery(id));
+			return NewResult(response);
+		}
+		#endregion
+	}
+}
--- EventManagement.Data/AppMetaData/Router.cs
+++ EventManagement.Data/AppMetaData/Router.cs
@@ -17,5 +17,15 @@
 			public const string Paginated = Prefix+ "/Paginated";
 
 		}
+		public static class EventRouting
+		{
+			private const string Prefix = Rule + "/Event";
+			public const string List = Prefix + "/List";
+			public const string GetById = Prefix + SingleRoute;
+			public const string Create = Prefix + "/Create";
+			public const string Edit = Prefix;
+			public const string Paginated = Prefix + "/Paginated";
+
+		}
 	}
 }
