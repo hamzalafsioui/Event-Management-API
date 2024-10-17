899676796c8836ee82398419f4ef38dd8153fe72
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-17 13:24:59 +0000

    Fix Migration CreatorUserId

--- EventManagement.Infrustructure/Migrations/20241016214221_InitialCreate.cs
+++ EventManagement.Infrustructure/Migrations/20241016214221_InitialCreate.cs
@@ -1,198 +1,197 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace EventManagement.Infrustructure.Migrations
 {
-    /// <inheritdoc />
-    public partial class InitialCreate : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Categories",
-                columns: table => new
-                {
-                    CategoryId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
-                });
+	/// <inheritdoc />
+	public partial class InitialCreate : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+				name: "Categories",
+				columns: table => new
+				{
+					CategoryId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Categories", x => x.CategoryId);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Role = table.Column<int>(type: "int", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastLoginDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.UserId);
-                });
+			migrationBuilder.CreateTable(
+				name: "Users",
+				columns: table => new
+				{
+					UserId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+					Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+					Role = table.Column<int>(type: "int", nullable: false),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					LastLoginDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+					IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Users", x => x.UserId);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "Events",
-                columns: table => new
-                {
-                    EventId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    CategoryId = table.Column<int>(type: "int", nullable: false),
-                    CreatedBy = table.Column<int>(type: "int", nullable: false),
-                    Capacity = table.Column<int>(type: "int", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    CreatorUserId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Events", x => x.EventId);
-                    table.ForeignKey(
-                        name: "FK_Events_Categories_CategoryId",
-                        column: x => x.CategoryId,
-                        principalTable: "Categories",
-                        principalColumn: "CategoryId",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Events_Users_CreatorUserId",
-                        column: x => x.CreatorUserId,
-                        principalTable: "Users",
-                        principalColumn: "UserId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "Events",
+				columns: table => new
+				{
+					EventId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+					Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+					EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+					CategoryId = table.Column<int>(type: "int", nullable: false),
+					CreatedBy = table.Column<int>(type: "int", nullable: false),
+					Capacity = table.Column<int>(type: "int", nullable: false),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					//	CreatorUserId = table.Column<int>(type: "int", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Events", x => x.EventId);
+					table.ForeignKey(
+						name: "FK_Events_Categories_CategoryId",
+						column: x => x.CategoryId,
+						principalTable: "Categories",
+						principalColumn: "CategoryId",
+						onDelete: ReferentialAction.Cascade);
+					table.ForeignKey(
+						name: "FK_Events_Users_CreatedBy",
+						column: x => x.CreatedBy,
+						principalTable: "Users",
+						principalColumn: "UserId",
+						onDelete: ReferentialAction.Cascade);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "Attendees",
-                columns: table => new
-                {
-                    AttendeeId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    EventId = table.Column<int>(type: "int", nullable: false),
-                    UserId = table.Column<int>(type: "int", nullable: false),
-                    Status = table.Column<int>(type: "int", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    RSVPDate = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    HasAttended = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Attendees", x => x.AttendeeId);
-                    table.ForeignKey(
-                        name: "FK_Attendees_Events_EventId",
-                        column: x => x.EventId,
-                        principalTable: "Events",
-                        principalColumn: "EventId",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Attendees_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "UserId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "Attendees",
+				columns: table => new
+				{
+					AttendeeId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					EventId = table.Column<int>(type: "int", nullable: false),
+					UserId = table.Column<int>(type: "int", nullable: false),
+					Status = table.Column<int>(type: "int", nullable: false),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					RSVPDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+					HasAttended = table.Column<bool>(type: "bit", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Attendees", x => x.AttendeeId);
+					table.ForeignKey(
+						name: "FK_Attendees_Events_EventId",
+						column: x => x.EventId,
+						principalTable: "Events",
+						principalColumn: "EventId",
+						onDelete: ReferentialAction.Restrict);
+					table.ForeignKey(
+						name: "FK_Attendees_Users_UserId",
+						column: x => x.UserId,
+						principalTable: "Users",
+						principalColumn: "UserId",
+						onDelete: ReferentialAction.Restrict);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "Comments",
-                columns: table => new
-                {
-                    CommentId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    EventId = table.Column<int>(type: "int", nullable: false),
-                    UserId = table.Column<int>(type: "int", nullable: false),
-                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Status = table.Column<int>(type: "int", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Comments", x => x.CommentId);
-                    table.ForeignKey(
-                        name: "FK_Comments_Events_EventId",
-                        column: x => x.EventId,
-                        principalTable: "Events",
-                        principalColumn: "EventId",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Comments_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "UserId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "Comments",
+				columns: table => new
+				{
+					CommentId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					EventId = table.Column<int>(type: "int", nullable: false),
+					UserId = table.Column<int>(type: "int", nullable: false),
+					Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					Status = table.Column<int>(type: "int", nullable: false),
+					CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+					UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Comments", x => x.CommentId);
+					table.ForeignKey(
+						name: "FK_Comments_Events_EventId",
+						column: x => x.EventId,
+						principalTable: "Events",
+						principalColumn: "EventId",
+						onDelete: ReferentialAction.Restrict);
+					table.ForeignKey(
+						name: "FK_Comments_Users_UserId",
+						column: x => x.UserId,
+						principalTable: "Users",
+						principalColumn: "UserId",
+						onDelete: ReferentialAction.Restrict);
+				});
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Attendees_EventId",
-                table: "Attendees",
-                column: "EventId");
+			migrationBuilder.CreateIndex(
+				name: "IX_Attendees_EventId",
+				table: "Attendees",
+				column: "EventId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Attendees_UserId",
-                table: "Attendees",
-                column: "UserId");
+			migrationBuilder.CreateIndex(
+				name: "IX_Attendees_UserId",
+				table: "Attendees",
+				column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Comments_EventId",
-                table: "Comments",
-                column: "EventId");
+			migrationBuilder.CreateIndex(
+				name: "IX_Comments_EventId",
+				table: "Comments",
+				column: "EventId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Comments_UserId",
-                table: "Comments",
-                column: "UserId");
+			migrationBuilder.CreateIndex(
+				name: "IX_Comments_UserId",
+				table: "Comments",
+				column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Events_CategoryId",
-                table: "Events",
-                column: "CategoryId");
+			migrationBuilder.CreateIndex(
+				name: "IX_Events_CategoryId",
+				table: "Events",
+				column: "CategoryId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Events_CreatorUserId",
-                table: "Events",
-                column: "CreatorUserId");
-        }
+			migrationBuilder.CreateIndex(
+				name: "IX_Events_CreatorUserId",
+				table: "Events",
+				column: "CreatorUserId");
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Attendees");
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "Attendees");
 
-            migrationBuilder.DropTable(
-                name: "Comments");
+			migrationBuilder.DropTable(
+				name: "Comments");
 
-            migrationBuilder.DropTable(
-                name: "Events");
+			migrationBuilder.DropTable(
+				name: "Events");
 
-            migrationBuilder.DropTable(
-                name: "Categories");
+			migrationBuilder.DropTable(
+				name: "Categories");
 
-            migrationBuilder.DropTable(
-                name: "Users");
-        }
-    }
+			migrationBuilder.DropTable(
+				name: "Users");
+		}
+	}
 }
