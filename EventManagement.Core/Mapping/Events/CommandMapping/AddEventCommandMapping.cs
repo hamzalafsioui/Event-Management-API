829065fb9e9fd47b20038a5e8753f442eef6edad
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 20:15:50 +0000

    CommandMapping for Add Event

---
+++ EventManagement.Core/Mapping/Events/CommandMapping/AddEventCommandMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventManagement.Core.Features.Events.Commands.Models;
+using EventManagement.Data.Entities;
+
+namespace EventManagement.Core.Mapping.Events
+{
+	public partial class EventProfile : Profile
+	{
+		private void AddEventCommandMapping()
+		{
+			CreateMap<AddEventCommand, Event>();
+		}
+	}
+}
