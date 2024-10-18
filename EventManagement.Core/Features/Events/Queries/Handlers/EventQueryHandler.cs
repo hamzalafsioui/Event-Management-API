af8745f81c3932d80b62cc588e4f35dbc4195b62
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 12:28:49 +0000

    Implement Hanlde for getEventById

--- EventManagement.Core/Features/Events/Queries/Handlers/EventQueryHandler.cs
+++ EventManagement.Core/Features/Events/Queries/Handlers/EventQueryHandler.cs
@@ -3,9 +3,12 @@ using EventManagement.Core.Bases;
 using EventManagement.Core.Features.Events.Queries.Models;
 using EventManagement.Core.Features.Events.Queries.Response;
 using EventManagement.Core.Resources;
+using EventManagement.Core.Wrappers;
+using EventManagement.Data.Entities;
 using EventManagement.Service.Abstracts;
 using MediatR;
 using Microsoft.Extensions.Localization;
+using System.Linq.Expressions;
 
 namespace EventManagement.Core.Features.Events.Queries.Handlers
 {
@@ -16,14 +19,18 @@ namespace EventManagement.Core.Features.Events.Queries.Handlers
 		private readonly IStringLocalizer<SharedResources> _stringLocalizer;
 		private readonly IEventService _eventService;
 		private readonly IMapper _mapper;
+		private readonly IAttendeeService _attendeeService;
+		private readonly ICommentService _commentService;
 		#endregion
 
 		#region Consructors
-		public EventQueryHandler(IStringLocalizer<SharedResources> stringLocalizer, IEventService eventService, IMapper mapper) : base(stringLocalizer)
+		public EventQueryHandler(IStringLocalizer<SharedResources> stringLocalizer, IEventService eventService, IMapper mapper, IAttendeeService attendeeService, ICommentService commentService) : base(stringLocalizer)
 		{
 			_stringLocalizer = stringLocalizer;
 			_eventService = eventService;
 			_mapper = mapper;
+			_attendeeService = attendeeService;
+			_commentService = commentService;
 		}
 
 
@@ -42,6 +49,18 @@ namespace EventManagement.Core.Features.Events.Queries.Handlers
 
 			// mapping 
 			var eventMapper = _mapper.Map<GetEventByIdResponse>(response);
+
+			// Pagination
+			Expression<Func<Attendee, AttendeeResponse>> expressionAttend = eA => new AttendeeResponse(eA.AttendeeId, eA.User.Username);
+			Expression<Func<Comment, CommentResponse>> expressionComment = eC => new CommentResponse(eC.CommentId, eC.User.Username, eC.Content);
+			var AttendeesQueryable = _attendeeService.GetAttendeesByEventIdQueryable(request.Id);
+			var CommentsQueryable = _commentService.GetCommentsByEventIdQueryable(request.Id);
+
+			var AttendePaginatedList = await AttendeesQueryable.Select(expressionAttend).ToPaginatedListAsync(request.AttendeePageNumber, request.AttendeePageSize);
+			var CommentPaginatedList = await CommentsQueryable.Select(expressionComment).ToPaginatedListAsync(request.CommentPageNumber, request.CommentPageSize);
+			eventMapper.AttendeesList = AttendePaginatedList;
+			eventMapper.CommentsList = CommentPaginatedList;
+
 			// response 
 			return Success(eventMapper);
 
