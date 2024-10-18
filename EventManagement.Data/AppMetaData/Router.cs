6b9f77216b5445103a011fb88b23ce5ee6f31cb3
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 12:23:41 +0000

    change Router to be compatable with [FromQuery]

--- EventManagement.Data/AppMetaData/Router.cs
+++ EventManagement.Data/AppMetaData/Router.cs
@@ -14,14 +14,14 @@
 			public const string GetById = Prefix + SingleRoute;
 			public const string Create = Prefix + "/Create";
 			public const string Edit = Prefix;
-			public const string Paginated = Prefix+ "/Paginated";
+			public const string Paginated = Prefix + "/Paginated";
 
 		}
 		public static class EventRouting
 		{
 			private const string Prefix = Rule + "/Event";
 			public const string List = Prefix + "/List";
-			public const string GetById = Prefix + SingleRoute;
+			public const string GetById = Prefix;
 			public const string Create = Prefix + "/Create";
 			public const string Edit = Prefix;
 			public const string Paginated = Prefix + "/Paginated";
