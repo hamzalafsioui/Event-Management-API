bc570c69a891e30d31464c2f362b5607f433be97
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 12:30:42 +0000

    Comment all things relation mapping with Attendee & comment

--- EventManagement.Core/Mapping/Events/QueryMapping/GetEventByIdMapping.cs
+++ EventManagement.Core/Mapping/Events/QueryMapping/GetEventByIdMapping.cs
@@ -11,18 +11,18 @@ namespace EventManagement.Core.Mapping.Events
 		{
 			CreateMap<Event, GetEventByIdResponse>()
 				.ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
-				.ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.Creator != null ? src.Creator.Username : "Unknown"))
-				.ForMember(dest => dest.AttendeesList, opt => opt.MapFrom(src => src.Attendees))
-				.ForMember(dest => dest.CommentsList, opt => opt.MapFrom(src => src.Comments));
+				.ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.Creator != null ? src.Creator.Username : "Unknown"));
+			//	.ForMember(dest => dest.AttendeesList, opt => opt.MapFrom(src => src.Attendees))
+			//.ForMember(dest => dest.CommentsList, opt => opt.MapFrom(src => src.Comments));
 
-			CreateMap<Attendee, AttendeeResponse>()
-				.ForMember(dest => dest.AttendeeId, opt => opt.MapFrom(src => src.AttendeeId))
-				.ForMember(dest => dest.AttendeeName, opt => opt.MapFrom(src => src.User.Username));
+			//CreateMap<Attendee, AttendeeResponse>()
+			//	.ForMember(dest => dest.AttendeeId, opt => opt.MapFrom(src => src.AttendeeId))
+			//	.ForMember(dest => dest.AttendeeName, opt => opt.MapFrom(src => src.User.Username));
 
-			CreateMap<Comment, CommentResponse>()
-				.ForMember(dest => dest.CommentId, opt => opt.MapFrom(src => src.CommentId))
-				.ForMember(dest => dest.WriteBy, opt => opt.MapFrom(src => src.User != null ? src.User.Username : "Anonymous"))
-				.ForMember(dest => dest.Content, opt => opt.MapFrom(src => src.Content));
+			//CreateMap<Comment, CommentResponse>()
+			//	.ForMember(dest => dest.CommentId, opt => opt.MapFrom(src => src.CommentId))
+			//	.ForMember(dest => dest.WriteBy, opt => opt.MapFrom(src => src.User != null ? src.User.Username : "Anonymous"))
+			//	.ForMember(dest => dest.Content, opt => opt.MapFrom(src => src.Content));
 		}
 	}
 }
