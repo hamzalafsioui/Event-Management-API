53e8e62ce902600da9242cb2175e663e0a08026e
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 12:25:34 +0000

    add attendeeResponse & commentResponse

--- EventManagement.Core/Features/Events/Queries/Response/GetEventByIdResponse.cs
+++ EventManagement.Core/Features/Events/Queries/Response/GetEventByIdResponse.cs
@@ -1,4 +1,5 @@
-﻿using EventManagement.Data.Entities;
+﻿using EventManagement.Core.Wrappers;
+using EventManagement.Data.Entities;
 
 namespace EventManagement.Core.Features.Events.Queries.Response
 {
@@ -16,19 +17,34 @@ namespace EventManagement.Core.Features.Events.Queries.Response
 
 		public DateTime CreatedAt { get; set; }
 
-		public List<AttendeeResponse> AttendeesList {  get; set; }
-		public List<CommentResponse> CommentsList {  get; set; }
+		public PaginatedResult<AttendeeResponse>? AttendeesList {  get; set; }
+		public PaginatedResult<CommentResponse>? CommentsList {  get; set; }
 
 	}
 	public class AttendeeResponse
 	{
+	
+
 		public int AttendeeId { get; set; }
 		public string AttendeeName { get; set; }
+		public AttendeeResponse(int attendeeId, string attendeeName)
+		{
+			AttendeeId = attendeeId;
+			AttendeeName = attendeeName;
+		}
 	}
 	public class CommentResponse
 	{
+	
 		public int CommentId { get; set; }
 		public string WriteBy { get; set; }
 		public string Content {  get; set; }
+		public CommentResponse(int commentId, string writeBy, string content)
+		{
+			CommentId = commentId;
+			WriteBy = writeBy;
+			Content = content;
+		}
+
 	}
 }
