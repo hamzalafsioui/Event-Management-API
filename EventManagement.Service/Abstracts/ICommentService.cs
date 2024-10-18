a692016601f10ab1625a55a7ede97dda12051034
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-18 12:18:01 +0000

    CommentService

---
+++ EventManagement.Service/Abstracts/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Data.Entities;
+
+namespace EventManagement.Service.Abstracts
+{
+	public interface ICommentService
+	{
+		public IQueryable<Comment> GetCommentsByEventIdQueryable(int eventId);
+	}
+}
---
+++ EventManagement.Service/Implementations/CommentService.cs
@@ -0,0 +1,27 @@
+﻿using EventManagement.Data.Entities;
+using EventManagement.Infrustructure.Repositories;
+using EventManagement.Service.Abstracts;
+
+namespace EventManagement.Service.Implementations
+{
+	public class CommentService : ICommentService
+	{
+		private readonly ICommentRepository _commentRepository;
+		#region Fields
+
+		#endregion
+		#region Constructors
+		public CommentService(ICommentRepository commentRepository)
+		{
+			_commentRepository = commentRepository;
+		}
+		#endregion
+		#region Actions
+		public IQueryable<Comment> GetCommentsByEventIdQueryable(int eventId)
+		{
+			return _commentRepository.GetTableNoTracking().Where(e => e.EventId.Equals(eventId)).AsQueryable();
+		}
+		#endregion
+
+	}
+}
