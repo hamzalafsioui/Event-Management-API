fda86f58aa25181a785ef5f9540ac98e19ca9f66
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-19 12:51:25 +0000

    Add EventOrderingEnum.cs

---
+++ EventManagement.Data/Helper/EventOrderingEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace EventManagement.Data.Helper
+{
+	public enum EventOrderingEnum
+	{
+		EventId = 0,
+		Title = 1,
+		Location = 2,
+		StartTime = 3,
+		EndTime = 4,
+		Creator = 5,
+		CreatedAt = 6,
+	}
+}
