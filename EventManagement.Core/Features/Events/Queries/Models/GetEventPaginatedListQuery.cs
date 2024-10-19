59f61b38b807e8dd9d0e2d8658e9ced2380525bd
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-19 22:34:37 +0000

    Implement GetEventPaginatedListQuery for handle request

---
+++ EventManagement.Core/Features/Events/Queries/Models/GetEventPaginatedListQuery.cs
@@ -0,0 +1,15 @@
+﻿using EventManagement.Core.Features.Events.Queries.Response;
+using EventManagement.Core.Wrappers;
+using EventManagement.Data.Helper;
+using MediatR;
+
+namespace EventManagement.Core.Features.Events.Queries.Models
+{
+	public class GetEventPaginatedListQuery:IRequest<PaginatedResult<GetEventPaginatedListResponse>>
+	{
+		public int PageNumber { get; set; }
+		public int PageSize { get; set; }
+		public EventOrderingEnum OrderBy { get; set; }
+		public string? Search { get; set; }
+	}
+}
