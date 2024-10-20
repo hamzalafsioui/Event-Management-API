7d5dc166940d353bdc27e5b7edf4e300ee8fe730
hamzalafsioui <hamzalafsioui@gmail.com>
2024-10-20 12:02:37 +0000

    DeleteEventCommand as DTO

---
+++ EventManagement.Core/Features/Events/Commands/Models/DeleteEventCommand.cs
@@ -0,0 +1,19 @@
+﻿using EventManagement.Core.Bases;
+using MediatR;
+
+namespace EventManagement.Core.Features.Events.Commands.Models
+{
+	public class DeleteEventCommand : IRequest<Response<string>>
+	{
+		#region Fields
+		public int EventId { get; set; }
+		#endregion
+		#region Constructors
+		public DeleteEventCommand(int eventId)
+		{
+			EventId = eventId;
+		}
+		#endregion
+
+	}
+}
