131dc4285f112afcf1a85f5028dc47180b6c4791
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-03 21:35:53 +0000

    Implement UserClaimModel.cs to dealing with User Claims

---
+++ EventManagement.Data/Helper/UserClaimModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EventManagement.Data.Helper
+{
+	public class UserClaimModel
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Email { get; set; }
+	}
+}
