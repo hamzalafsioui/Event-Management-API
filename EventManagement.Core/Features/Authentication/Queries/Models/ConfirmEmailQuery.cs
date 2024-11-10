38d44550dab53f0104736b6ab5f696918e1260e9
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-10 21:51:11 +0000

    Confirm Email Query

---
+++ EventManagement.Core/Features/Authentication/Queries/Models/ConfirmEmailQuery.cs
@@ -0,0 +1,8 @@
+﻿using EventManagement.Core.Bases;
+using MediatR;
+
+namespace EventManagement.Core.Features.Authentication.Queries.Models
+{
+	public record ConfirmEmailQuery(int UserId,string Code):IRequest<Response<string>>;
+	
+}
