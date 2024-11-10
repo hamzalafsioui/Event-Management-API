02bd9087b78104b0d557099dc5a282bacb1b5cca
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-10 21:28:34 +0000

    Add Confirm Email EndPoint

--- EventManagement.API/Controllers/AuthenticationController.cs
+++ EventManagement.API/Controllers/AuthenticationController.cs
@@ -28,5 +28,11 @@ namespace EventManagement.API.Controllers
 			var response = await Mediator.Send(query);
 			return NewResult(response);
 		}
+		[HttpGet(Router.AuthenticationRouting.ConfirmEmail)]
+		public async Task<IActionResult> ConfirmEmail([FromQuery] ConfirmEmailQuery query)
+		{
+			var response = await Mediator.Send(query);
+			return NewResult(response);
+		}
 	}
 }
