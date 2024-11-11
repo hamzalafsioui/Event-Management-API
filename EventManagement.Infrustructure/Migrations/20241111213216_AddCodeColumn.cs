01aa289c2c5caa777535a61648572bd55cb8e4f8
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-11 21:56:40 +0000

    Add Column Code to Identity User

---
+++ EventManagement.Infrustructure/Migrations/20241111213216_AddCodeColumn.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagement.Infrustructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_AspNetUsers_UserId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_UserId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Events");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Events",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_UserId",
+                table: "Events",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_AspNetUsers_UserId",
+                table: "Events",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
