5fc9a019775678c8036f6be85eb74197cc1e1ef9
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-12 17:01:13 +0000

    Hash code & verify code

--- EventManagement.Data/Helper/Encryption/EncryptionHelper.cs
+++ EventManagement.Data/Helper/Encryption/EncryptionHelper.cs
@@ -24,33 +24,6 @@ namespace EventManagement.Data.Helper.Encryption
 				// Convert hash + salt to Base64
 				return Convert.ToBase64String(hashBytes);
 
-
-
-				//using (Aes aesAlg = Aes.Create())
-				//{
-				//	// Set the key and IV for AES encryption
-				//	aesAlg.Key = Encoding.UTF8.GetBytes(key);
-				//	aesAlg.IV = new byte[aesAlg.BlockSize / 8];
-
-
-				//	// Create an encryptor
-				//	ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-
-				//	// Encrypt the data
-				//	using (var msEncrypt = new System.IO.MemoryStream())
-				//	{
-				//		using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-				//		using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
-				//		{
-				//			swEncrypt.Write(plainText);
-				//		}
-
-
-				//		// Return the encrypted data as a Base64-encoded string
-				//		return Convert.ToBase64String(msEncrypt.ToArray());
-				//	}
-				//}
 			}
 		}
 		public static bool VerifyCode(string enteredCode, string storedHash)
@@ -77,27 +50,6 @@ namespace EventManagement.Data.Helper.Encryption
 
 			return true; // code match
 
-
-			//using (Aes aesAlg = Aes.Create())
-			//{
-			//	// Set the key and IV for AES decryption
-			//	aesAlg.Key = Encoding.UTF8.GetBytes(key);
-			//	aesAlg.IV = new byte[aesAlg.BlockSize / 8];
-
-
-			//	// Create a decryptor
-			//	ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-
-			//	// Decrypt the data
-			//	using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
-			//	using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-			//	using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
-			//	{
-			//		// Read the decrypted data from the StreamReader
-			//		return srDecrypt.ReadToEnd();
-			//	}
-			//}
 		}
 	}
 }
