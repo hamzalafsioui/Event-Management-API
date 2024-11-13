ebe473f2159a50b3e8c88dfb96ca6e4356117d78
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-13 18:00:37 +0000

    add GetUserEventEngagementSummaryMapping

---
+++ EventManagement.Core/Mapping/Users/QueryMapping/GetUserEventEngagementSummaryMapping.cs
@@ -0,0 +1,15 @@
+﻿using EventManagement.Core.Features.Users.Queries.Results;
+using EventManagement.Data.Entities.Views;
+
+namespace EventManagement.Core.Mapping.Users
+{
+	public partial class UserProfile
+	{
+		private void GetUserEventEngagementSummaryMapping()
+		{
+			CreateMap<ViewUserEventEngagementSummary, GetUserEventEngagementSummaryResponse>()
+			   .ForMember(destination => destination.LastCategoryAttended, option => option.MapFrom(source => source.LastCategoryAttended ?? "N/A"))
+			   .ForMember(destination => destination.LastEventAttendedDate, option => option.MapFrom(source => source.LastEventAttendedDate ?? DateTime.MinValue));
+		}
+	}
+}
