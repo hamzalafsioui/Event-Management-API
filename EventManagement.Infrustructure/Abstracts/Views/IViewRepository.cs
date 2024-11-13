9a72a5cc1ca9e16ace8b87d83bef653b2b364275
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-13 17:56:11 +0000

    viewRepository

---
+++ EventManagement.Infrustructure/Abstracts/Views/IViewRepository.cs
@@ -0,0 +1,8 @@
+﻿using EventManagement.Infrustructure.InfrustructureBase;
+
+namespace EventManagement.Infrustructure.Abstracts.IViewRepository
+{
+	public interface IViewRepository<T> : IGenericRepositoryAsync<T> where T : class
+	{
+	}
+}
