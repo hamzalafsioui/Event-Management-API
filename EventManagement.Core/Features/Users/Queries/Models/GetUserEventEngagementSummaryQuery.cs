a23a48fd6f23bd64dfa14feba7610f2cc07332bb
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-13 18:00:06 +0000

    add GetUserEventEngagementSummaryQuery

---
+++ EventManagement.Core/Features/Users/Queries/Models/GetUserEventEngagementSummaryQuery.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Core.Bases;
+using EventManagement.Core.Features.Users.Queries.Results;
+using MediatR;
+
+namespace EventManagement.Core.Features.Users.Queries.Models
+{
+	public record GetUserEventEngagementSummaryQuery : IRequest<Response<List<GetUserEventEngagementSummaryResponse>>>;
+	
+}
