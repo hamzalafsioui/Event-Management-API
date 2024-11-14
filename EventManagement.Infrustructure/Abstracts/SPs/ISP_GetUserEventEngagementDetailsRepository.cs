09110b08c01b6220f27d868f504b21a3738d320f
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-14 10:22:42 +0000

    Implement Repository for SP_GetUserEventEngagementDetails

---
+++ EventManagement.Infrustructure/Abstracts/SPs/ISP_GetUserEventEngagementDetailsRepository.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Data.Entities.SPs;
+
+namespace EventManagement.Infrustructure.Abstracts.SPs
+{
+	public interface ISP_GetUserEventEngagementDetailsRepository
+	{
+		public Task<SP_GetUserEventEngagementDetails> GetUserEventEngagementDetailsAsync(SP_GetUserEventEngagementDetailsParameters parameters);
+	}
+}
---
+++ EventManagement.Infrustructure/Repositories/SPs/SP_GetUserEventEngagementDetailsRepository.cs
@@ -0,0 +1,34 @@
+﻿using EventManagement.Data.Entities.SPs;
+using EventManagement.Infrustructure.Abstracts.SPs;
+using EventManagement.Infrustructure.Context;
+using StoredProcedureEFCore;
+
+namespace EventManagement.Infrustructure.Repositories.SPs
+{
+	public class SP_GetUserEventEngagementDetailsRepository : ISP_GetUserEventEngagementDetailsRepository
+	{
+		#region Fields
+		private readonly AppDbContext _appDbContext;
+		#endregion
+		#region Constructors
+		public SP_GetUserEventEngagementDetailsRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+		#endregion
+		#region Handle Functions
+
+		#endregion
+		public async Task<SP_GetUserEventEngagementDetails> GetUserEventEngagementDetailsAsync(SP_GetUserEventEngagementDetailsParameters parameters)
+		{
+			var result = new SP_GetUserEventEngagementDetails();
+			await _appDbContext.LoadStoredProc(nameof(SP_GetUserEventEngagementDetails))
+			   .AddParam(nameof(SP_GetUserEventEngagementDetails.UserId), parameters.UserId)
+			   .ExecAsync(async reader=>
+			   {
+				   result = await reader.FirstOrDefaultAsync<SP_GetUserEventEngagementDetails>();
+			} );
+			return result;
+		}
+	}
+}
