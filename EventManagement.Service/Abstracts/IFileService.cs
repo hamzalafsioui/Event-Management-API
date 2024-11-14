1095dcf175048773e61747e9deb41106e5a072eb
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-14 17:49:47 +0000

    Add FileService

---
+++ EventManagement.Service/Abstracts/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EventManagement.Service.Abstracts
+{
+	public interface IFileService
+	{
+		public Task<string> UploadImageAsync(string location, IFormFile file);
+	}
+}
---
+++ EventManagement.Service/Implementations/FileService.cs
@@ -0,0 +1,50 @@
+﻿using EventManagement.Service.Abstracts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace EventManagement.Service.Implementations
+{
+	public class FileService : IFileService
+	{
+		#region Fields
+		private readonly IWebHostEnvironment _webHostEnvironment;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		#endregion
+		#region Constructors
+		public FileService(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
+		{
+			_webHostEnvironment = webHostEnvironment;
+			_httpContextAccessor = httpContextAccessor;
+		}
+		#endregion
+		#region Actions
+		public async Task<string> UploadImageAsync(string folderName, IFormFile file)
+		{
+			if (file == null || file.Length == 0)
+				throw new ArgumentException("File cannot be empty", nameof(file));
+
+			var context = _httpContextAccessor?.HttpContext?.Request;
+			var baseURL = context?.Scheme + "://" + context?.Host;
+
+			// Ensure Folder Path
+			var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, folderName);
+			if (!Directory.Exists(folderPath))
+			{
+				Directory.CreateDirectory(folderPath);
+			}
+
+			// Generate unique file name
+			var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+			var fullPath = Path.Combine(folderPath, fileName);
+			using (var fileStream = new FileStream(fullPath, FileMode.Create))
+			{
+				await file.CopyToAsync(fileStream);
+			}
+
+			return $"{baseURL}/{folderName}/{fileName}";
+
+		}
+		#endregion
+
+	}
+}
