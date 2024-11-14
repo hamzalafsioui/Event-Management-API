7125e6a8956ee63ab0edc41f5d60a86ec3907b57
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-14 10:21:35 +0000

    Implement class SP_GetUserEventEngagementDetails to match SP in DB

---
+++ EventManagement.Data/Entities/SPs/SP_GetUserEventEngagementDetails.cs
@@ -0,0 +1,21 @@
+﻿namespace EventManagement.Data.Entities.SPs
+{
+	public class SP_GetUserEventEngagementDetails
+	{
+		public int UserId { get; set; }
+		public string UserName { get; set; }
+		public string Role { get; set; }
+		public int TotalEventsAttended { get; set; }
+		public int TotalCommentsMade { get; set; }
+		public int TotalEventsCreated { get; set; }
+		public int TotalEventsPhysicallyAttended { get; set; }
+		public int TotalEventsRelated { get; set; }
+		public string? LastCategoryAttended { get; set; }
+		public DateTime? LastEventAttendedDate { get; set; }
+	}
+	public class SP_GetUserEventEngagementDetailsParameters
+	{
+		public int UserId { get; set; } = 0; // Initial Value
+	}
+
+}
