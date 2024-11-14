606da6a241f1bdf1ebb6b05a571c000c8c690a54
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-14 10:26:37 +0000

    GetUserEventEngagementDetailsQuery for TRequest

---
+++ EventManagement.Core/Features/Users/Queries/Models/GetUserEventEngagementDetailsQuery.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Core.Bases;
+using EventManagement.Core.Features.Users.Queries.Results;
+using MediatR;
+
+namespace EventManagement.Core.Features.Users.Queries.Models
+{
+	public record GetUserEventEngagementDetailsQuery(int UserId):IRequest<Response<GetUserEventEngagementDetailsResponse>>;
+	
+}
