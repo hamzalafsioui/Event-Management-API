c0b07517430fba39dcd75e1ee5af969c4fbbd290
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-14 10:23:41 +0000

    add SP_GetUserEventEngagementDetailsRepository to dependencies container

--- EventManagement.Infrustructure/ModuleInfrustructureDependencies.cs
+++ EventManagement.Infrustructure/ModuleInfrustructureDependencies.cs
@@ -1,8 +1,10 @@
 ﻿using EventManagement.Data.Entities.Views;
 using EventManagement.Infrustructure.Abstracts;
 using EventManagement.Infrustructure.Abstracts.IViewRepository;
+using EventManagement.Infrustructure.Abstracts.SPs;
 using EventManagement.Infrustructure.InfrustructureBase;
 using EventManagement.Infrustructure.Repositories;
+using EventManagement.Infrustructure.Repositories.SPs;
 using EventManagement.Infrustructure.Repositories.Views;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,8 +22,12 @@ namespace EventManagement.Infrustructure
 			services.AddTransient<ICommentRepository, CommentRepository>();
 			services.AddTransient<ICategoryRepository, CategoryRepository>();
 			services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
+			
+			// views
 			services.AddTransient<IViewRepository<ViewUserEventEngagementSummary>, ViewUserEventEngagementSummaryRepository>();
 
+			// SPs
+			services.AddTransient<ISP_GetUserEventEngagementDetailsRepository, SP_GetUserEventEngagementDetailsRepository>();
 			return services;
 		}
 	}
