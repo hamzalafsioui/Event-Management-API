64a5cfbe4badb90f3425080498d9a33581b92cd8
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-17 21:05:10 +0000

    add Swagger documentation to the EmailController

--- EventManagement.API/Controllers/EmailController.cs
+++ EventManagement.API/Controllers/EmailController.cs
@@ -2,6 +2,7 @@
 using EventManagement.Core.Features.Emails.Commands.Models;
 using EventManagement.Data.AppMetaData;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace EventManagement.API.Controllers
 {
@@ -9,6 +10,15 @@ namespace EventManagement.API.Controllers
 	public class EmailController : AppControllerBase
 	{
 		[HttpPost(Router.EmailRouting.SendEmail)]
+		[SwaggerOperation(
+			Summary = "Send an email",
+			OperationId = "SendEmail",
+			Description = "<h3>Details</h3><p>Send an email using the specified command with all necessary details like recipient, subject, and body content.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public async Task<IActionResult> SendEmail([FromForm] SendEmailCommand command)
 		{
 			var response = await Mediator.Send(command);
