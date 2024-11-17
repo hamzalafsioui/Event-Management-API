d20008d06b2dccfaf1f35bfcd952d63469321417
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-17 21:04:31 +0000

    add Swagger documentation to the CategoryController

--- EventManagement.API/Controllers/CategoryController.cs
+++ EventManagement.API/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using EventManagement.Core.Features.Categories.Command.Models;
 using EventManagement.Core.Features.Categories.Queries.Models;
 using EventManagement.Data.AppMetaData;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace EventManagement.API.Controllers
 {
@@ -11,30 +12,75 @@ namespace EventManagement.API.Controllers
 	public class CategoryController : AppControllerBase
 	{
 		[HttpGet(Router.CategoryRouting.List)]
+		[SwaggerOperation(
+			Summary = "Get List of Categories",
+			OperationId = "GetCategoryList",
+			Description = "<h3>Details</h3><p>Retrieve a list of all categories available in the system.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
 		public async Task<IActionResult> GetCategoryList()
 		{
 			var response = await Mediator.Send(new GetCategoryListQuery());
 			return NewResult(response);
 		}
 		[HttpGet(Router.CategoryRouting.GetById)]
+		[SwaggerOperation(
+			Summary = "Get Category by ID",
+			OperationId = "GetCategoryById",
+			Description = "<h3>Details</h3><p>Retrieve the details of a category by its unique identifier.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
 		public async Task<IActionResult> GetCategoryById([FromRoute] int id)
 		{
 			var response = await Mediator.Send((new GetCategoryByIdQuery(id)));
 			return NewResult(response);
 		}
 		[HttpPost(Router.CategoryRouting.Create)]
+		[SwaggerOperation(
+			Summary = "Create a New Category",
+			OperationId = "CreateCategory",
+			Description = "<h3>Details</h3><p>Create a new category with the specified details.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
 		public async Task<IActionResult> Create([FromBody] AddCategoryCommand command)
 		{
 			var response = await Mediator.Send(command);
 			return NewResult(response);
 		}
 		[HttpPut(Router.CategoryRouting.Edit)]
+		[SwaggerOperation(
+			Summary = "Edit Existing Category",
+			OperationId = "EditCategory",
+			Description = "<h3>Details</h3><p>Update the details of an existing category.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
 		public async Task<IActionResult> Edit([FromBody] EditCategoryCommand command)
 		{
 			var response = await Mediator.Send(command);
 			return NewResult(response);
 		}
 		[HttpDelete(Router.CategoryRouting.Delete)]
+		[SwaggerOperation(
+			Summary = "Delete Category",
+			OperationId = "DeleteCategory",
+			Description = "<h3>Details</h3><p>Delete a category by its unique identifier.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
 		public async Task<IActionResult> Delete([FromRoute] int id)
 		{
 			var response = await Mediator.Send(new DeleteCategoryCommand(id));
