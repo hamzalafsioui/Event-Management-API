b937662d0f89e705f5b3c7ce5227bcb62bd017f0
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-18 18:37:17 +0000

    XML Documentation to IEmailService

--- EventManagement.Service/Abstracts/IEmailService.cs
+++ EventManagement.Service/Abstracts/IEmailService.cs
@@ -1,7 +1,23 @@
 ﻿namespace EventManagement.Service.Abstracts
 {
+	/// <summary>
+	/// Interface defining email-related service operations.
+	/// </summary>
 	public interface IEmailService
 	{
+		/// <summary>
+		/// Sends an email asynchronously to the specified <paramref name="toEmail"/> with the provided <paramref name="message"/> and <paramref name="subject"/>.
+		/// </summary>
+		/// <param name="toEmail">The recipient's email address.</param>
+		/// <param name="message">The content of the email message in HTML format.</param>
+		/// <param name="subject">The subject of the email.</param>
+		/// <returns>
+		/// A <see cref="string"/> result indicating the outcome of the operation:
+		/// <list type="bullet">
+		/// <item><description><c>"Success"</c>: If the email was sent successfully.</description></item>
+		/// <item><description><c>"Failed"</c>: If an error occurred during the email sending process.</description></item>
+		/// </list>
+		/// </returns>
 		public Task<string> SendEmailAsync(string email, string message, string subject);
 	}
 }
