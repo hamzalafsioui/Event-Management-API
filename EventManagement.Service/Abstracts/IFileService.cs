fc6223dd94a2cf23cd1347e6f9b80123e910c5d1
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-18 18:36:18 +0000

    XML Documentation to IFileService

--- EventManagement.Service/Abstracts/IFileService.cs
+++ EventManagement.Service/Abstracts/IFileService.cs
@@ -2,8 +2,21 @@
 
 namespace EventManagement.Service.Abstracts
 {
+	/// <summary>
+	/// Interface defining file management operations, such as image uploads.
+	/// </summary>
 	public interface IFileService
 	{
+		/// <summary>
+		/// Uploads an image file to the specified folder asynchronously, optionally replacing an old file.
+		/// </summary>
+		/// <param name="folderName">The name of the folder where the image will be uploaded.</param>
+		/// <param name="file">The <see cref="IFormFile"/> representing the image file to upload.</param>
+		/// <param name="oldFile">The name of the old file to replace, if applicable. Can be <see langword="null"/>.</param>
+		/// <returns>
+		/// A task that represents the asynchronous operation. The task result contains the path to the uploaded file as a <see cref="string"/>.
+		/// </returns>
+
 		public Task<string> UploadImageAsync(string folderName, IFormFile file, string? oldFile);
 	}
 }
