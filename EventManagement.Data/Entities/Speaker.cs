36134f6bc1f6290ffb5a99faf1652aa49c3c36f2
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-19 22:04:09 +0000

    Entity Speaker

---
+++ EventManagement.Data/Entities/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using EventManagement.Data.Entities.Identity;
+
+namespace EventManagement.Data.Entities
+{
+	public class Speaker
+	{
+		public int Id { get; set; }
+
+		public int UserId { get; set; }
+		public User User { get; set; }
+
+		public string Bio { get; set; }
+
+		public ICollection<SpeakerEvent> SpeakerEvents { get; set; } = new List<SpeakerEvent>();
+
+	}
+}
