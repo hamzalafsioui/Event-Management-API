7f3a6b7a19d676aa123f0ce4717ff3a5c244781c
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-19 22:05:03 +0000

    Add SpeakerEvent for relation mant to many between Event & speaker

---
+++ EventManagement.Data/Entities/SpeakerEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace EventManagement.Data.Entities
+{
+	public class SpeakerEvent
+	{
+		public int EventId { get; set; }
+		public Event Event { get; set; } 
+
+		public int SpeakerId { get; set; }
+		public Speaker Speaker { get; set; } 
+
+	}
+}
