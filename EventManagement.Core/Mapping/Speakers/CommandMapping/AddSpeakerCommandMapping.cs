7e60ef690a2d323e57deb65ef5f3f21d12e0312b
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 12:50:44 +0000

    Add SpeakerCommand Mapping

---
+++ EventManagement.Core/Mapping/Speakers/CommandMapping/AddSpeakerCommandMapping.cs
@@ -0,0 +1,13 @@
+﻿using EventManagement.Core.Features.Speakers.Commands.Models;
+using EventManagement.Data.Entities;
+
+namespace EventManagement.Core.Mapping.Speakers
+{
+	public partial class SpeakerProfile
+	{
+		private void AddSpeakerCommandMapping()
+		{
+			CreateMap<AddSpeakerCommand, Speaker>();
+		}
+	}
+}
