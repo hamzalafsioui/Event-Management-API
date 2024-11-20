fe6e815157dc6fb0a9172ec3c1d929b5fde5ff55
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 12:53:20 +0000

    Add SpeakerController with Endpoint Create

---
+++ EventManagement.API/Controllers/SpeakerController.cs
@@ -0,0 +1,32 @@
+﻿using EventManagement.API.Base;
+using EventManagement.Core.Features.Speakers.Commands.Models;
+using EventManagement.Data.AppMetaData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace EventManagement.API.Controllers
+{
+	[ApiController]
+	public class SpeakerController : AppControllerBase
+	{
+		#region Actions
+
+		[HttpPost(Router.SpeakerRouting.Create)]
+		[AllowAnonymous]
+		[SwaggerOperation(
+			Summary = "Create Speaker",
+			OperationId = "CreateSpeaker",
+			Description = "<h3>Details</h3><p>Create a new Speaker with the specified details.</p>"
+		)]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<IActionResult> Create([FromForm] AddSpeakerCommand command)
+		{
+			var response = await Mediator.Send(command);
+			return NewResult(response);
+		}
+		#endregion
+
+	}
+}
