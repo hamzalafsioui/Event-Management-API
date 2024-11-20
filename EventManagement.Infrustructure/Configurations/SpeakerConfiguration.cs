2a3e251bb775f0ce29ad8941afb1ef9a69dbc869
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 12:45:50 +0000

    SpeakerConfiguration

---
+++ EventManagement.Infrustructure/Configurations/SpeakerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EventManagement.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventManagement.Infrustructure.Configurations
+{
+	public class SpeakerConfiguration : IEntityTypeConfiguration<Speaker>
+	{
+		public void Configure(EntityTypeBuilder<Speaker> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.HasOne(x => x.User)
+				.WithMany()
+				.HasForeignKey(x => x.UserId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			builder.HasIndex(x => x.UserId)
+			   .IsUnique()
+			   .HasDatabaseName("IX_Speaker_UserId");
+
+
+			builder.ToTable("Speakers");
+		}
+	}
+}
