23cd854bc505d689d4179264108bbdee4573ec53
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 19:49:20 +0000

    Implement DTO GetSpeakerByIdQueryResponse

---
+++ EventManagement.Core/Features/Speakers/Queries/Responses/GetSpeakerByIdQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EventManagement.Core.Features.Speakers.Queries.Responses
+{
+	public class GetSpeakerByIdQueryResponse
+	{
+		public int SpeakerId { get; set; }
+		public string FullName { get; set; }
+		public string Bio { get; set; }
+	}
+}
