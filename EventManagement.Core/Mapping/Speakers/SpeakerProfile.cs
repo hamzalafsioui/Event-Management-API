e4527118e706f73bffe5c1843ef1a7559ae368e5
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 12:51:26 +0000

    SpeakerProfile for Speaker Mapping

---
+++ EventManagement.Core/Mapping/Speakers/SpeakerProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace EventManagement.Core.Mapping.Speakers
+{
+	public partial class SpeakerProfile : Profile
+	{
+		public SpeakerProfile()
+		{
+			AddSpeakerCommandMapping();
+		}
+	}
+}
