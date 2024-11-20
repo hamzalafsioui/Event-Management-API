1e9eb7b484659cee48cb1dd7632cc894584fefe1
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 12:41:22 +0000

    Implement SpeakerService

---
+++ EventManagement.Service/Abstracts/ISpeakerService.cs
@@ -0,0 +1,57 @@
+﻿using EventManagement.Data.Entities;
+using EventManagement.Data.Helper;
+
+namespace EventManagement.Service.Abstracts
+{
+	public interface ISpeakerService
+	{
+		/// <summary>
+		/// Retrieves a list of all <see cref="Speaker"/> entities asynchronously.
+		/// </summary>
+		/// <returns>A task that represents the asynchronous operation. The task result contains a <see cref="List{T}"/> of <see cref="Speaker"/> entities.</returns>
+		public Task<List<Speaker>> GetSpeakersListAsync();
+
+		/// <summary>
+		/// Retrieves a <see cref="Speaker"/> by its <paramref name="id"/> asynchronously.
+		/// </summary>
+		/// <param name="id">The unique identifier of the <see cref="Speaker"/>.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the <see cref="Speaker"/> entity, or <see langword="null"/> if not found.</returns>
+		public Task<Speaker?> GetByIdAsync(int id);
+
+
+		/// <summary>
+		/// Adds a new speaker to the speakers.
+		/// </summary>
+		/// <param name="attendee">The speaker entity to be added.</param>
+		/// <returns>
+		/// A task representing the asynchronous operation, with a result of the added <see cref="Speaker"/>.
+		/// </returns>
+		Task<Result> AddAsync(Speaker speaker);
+
+		/// <summary>
+		/// Updates an existing speaker's details.
+		/// </summary>
+		/// <param name="speaker">The speaker entity with updated information.</param>
+		/// <returns>
+		/// A task representing the asynchronous operation, with a result of the updated <see cref="Speaker"/>.
+		/// </returns>
+		Task<Speaker> UpdateAsyc(Speaker speaker);
+
+		/// <summary>
+		/// Deletes the specified speaker from the speakers.
+		/// </summary>
+		/// <param name="speaker">The speaker entity to be deleted.</param>
+		/// <returns>
+		/// A task representing the asynchronous operation, with a boolean indicating whether the deletion was successful.
+		/// </returns>
+		Task<bool> DeleteAsync(Speaker speaker);
+
+
+		/// <summary>
+		/// Checks if a <paramref name="userId"/> exists in the speakers.
+		/// </summary>
+		/// <param name="userId">The <paramref name="userId"/> to check.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains <see langword="true"/> if the user exists by userId; otherwise, <see langword="false"/>.</returns>
+		Task<bool> IsUserExist(int userId);
+	}
+}
---
+++ EventManagement.Service/Implementations/SpeakerService.cs
@@ -0,0 +1,97 @@
+﻿using EventManagement.Data.Entities;
+using EventManagement.Data.Entities.Identity;
+using EventManagement.Data.Helper;
+using EventManagement.Data.Helper.Enums;
+using EventManagement.Infrustructure.Abstracts;
+using EventManagement.Service.Abstracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace EventManagement.Service.Implementations
+{
+	public class SpeakerService : ISpeakerService
+	{
+		#region Fields
+		private readonly ISpeakerRepository _speakerRepository;
+		private readonly UserManager<User> _userManager;
+		#endregion
+		#region Constructors
+		public SpeakerService(ISpeakerRepository speakerRepository,
+			UserManager<User> userManager)
+		{
+			_speakerRepository = speakerRepository;
+			_userManager = userManager;
+		}
+
+
+		#endregion
+
+		#region Actions
+		public async Task<List<Speaker>> GetSpeakersListAsync()
+		{
+			return await _speakerRepository.GetSpeakersListAsync();
+		}
+
+		public async Task<Speaker?> GetByIdAsync(int id) => await _speakerRepository.GetByIdAsync(id);
+
+
+		public async Task<Result> AddAsync(Speaker speaker)
+		{
+			using var transaction = await _speakerRepository.BeginTransactionAsync();
+
+			try
+			{
+				// Add speaker
+				var newSpeaker = await _speakerRepository.AddAsync(speaker);
+				if (newSpeaker == null)
+					return Result.Failure("Failed to add the speaker.");
+
+				// Update user role
+				var user = await _userManager.FindByIdAsync(speaker.UserId.ToString());
+				if (user == null)
+					return Result.Failure("User not found.");
+
+				user.Role = UserRoleEnum.Speaker;
+				var updateUserRoleResult = await _userManager.UpdateAsync(user);
+				if (!updateUserRoleResult.Succeeded)
+				{
+					// Rollback speaker addition if role update fails
+					await _speakerRepository.DeleteAsync(newSpeaker);
+					return Result.Failure("Failed to update the user's role.");
+				}
+
+				// Add "Speaker" role to user's roles
+				var addToSpeakerRoleResult = await _userManager.AddToRoleAsync(user, "Speaker");
+				if (!addToSpeakerRoleResult.Succeeded)
+				{
+					// Rollback speaker addition if role assignment fails
+					await _speakerRepository.DeleteAsync(newSpeaker);
+					return Result.Failure("Failed to assign the Speaker role.");
+				}
+
+				await transaction.CommitAsync();
+				return Result.Success();
+			}
+			catch (Exception ex)
+			{
+				await transaction.RollbackAsync();
+				Log.Error($"Error in Speaker AddAsync: {ex.Message}");
+				return Result.Failure("An unexpected error occurred.");
+			}
+		}
+		public async Task<Speaker> UpdateAsyc(Speaker speaker) => await _speakerRepository.UpdateAsync(speaker);
+		public async Task<bool> DeleteAsync(Speaker speaker) => await _speakerRepository.DeleteAsync(speaker);
+
+		public async Task<bool> IsUserExist(int userId)
+		{
+			var result = await _speakerRepository.GetTableNoTracking().FirstOrDefaultAsync(x => x.UserId.Equals(userId));
+			return result != null;
+		}
+
+
+
+
+		#endregion
+	}
+}
