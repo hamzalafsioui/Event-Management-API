687eefba62ed0cdd0997cf71bd7623fb5694e420
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 19:07:03 +0000

    Create GetSpeakerListResponse

---
+++ EventManagement.Core/Features/Speakers/Queries/Responses/GetSpeakerListResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EventManagement.Core.Features.Speakers.Queries.Responses
+{
+	public class GetSpeakerListResponse
+	{
+		public int SpeakerId { get; set; }
+		public string FullName { get; set; }
+		public string Bio { get; set; }
+	}
+}
