d17630672d39c0fa7514e2f75c06735902dd2d98
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 20:44:38 +0000

    DTO for Update Speaker

---
+++ EventManagement.Core/Features/Speakers/Commands/Models/EditSpeakerCommand.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Core.Bases;
+using MediatR;
+
+namespace EventManagement.Core.Features.Speakers.Commands.Models
+{
+	public record EditSpeakerCommand(int SpeakerId, string Bio) : IRequest<Response<string>>;
+
+
+}
