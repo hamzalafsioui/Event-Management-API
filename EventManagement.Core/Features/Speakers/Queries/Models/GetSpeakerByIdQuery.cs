06fa3c527f80bc86bd916a834064a27af0e4d1d3
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 19:48:38 +0000

    Implement DTO GetSpeakerByIdQuery

---
+++ EventManagement.Core/Features/Speakers/Queries/Models/GetSpeakerByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using EventManagement.Core.Bases;
+using EventManagement.Core.Features.Speakers.Queries.Responses;
+using MediatR;
+
+namespace EventManagement.Core.Features.Speakers.Queries.Models
+{
+	public record GetSpeakerByIdQuery(int speakerId):IRequest<Response<GetSpeakerByIdQueryResponse>>;
+	
+}
