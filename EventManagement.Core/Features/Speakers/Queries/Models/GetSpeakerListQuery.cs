838637e75343b9e4899838a510331604b05fb2e0
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-20 19:06:41 +0000

    Create DTO GetSpeakerListQuery

---
+++ EventManagement.Core/Features/Speakers/Queries/Models/GetSpeakerListQuery.cs
@@ -0,0 +1,10 @@
+﻿using EventManagement.Core.Bases;
+using EventManagement.Core.Features.Speakers.Queries.Responses;
+using MediatR;
+
+namespace EventManagement.Core.Features.Speakers.Queries.Models
+{
+	public record GetSpeakerListQuery() : IRequest<Response<List<GetSpeakerListResponse>>>;
+
+
+}
