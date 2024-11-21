cebd952d3ac449f713cc1fff88e5631251b25263
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-21 23:23:07 +0000

    SpeakerEventRepository

---
+++ EventManagement.Infrustructure/ISpeakerEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using EventManagement.Data.Entities;
+using EventManagement.Infrustructure.InfrustructureBase;
+
+namespace EventManagement.Infrustructure
+{
+	public interface ISpeakerEventRepository : IGenericRepositoryAsync<SpeakerEvent>
+	{
+		public Task<List<SpeakerEvent>> GetSpeakerEventListAsync();
+	}
+}
---
+++ EventManagement.Infrustructure/Repositories/SpeakerEventRepository.cs
@@ -0,0 +1,29 @@
+﻿using EventManagement.Data.Entities;
+using EventManagement.Infrustructure.Context;
+using EventManagement.Infrustructure.InfrustructureBase;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagement.Infrustructure.Repositories
+{
+	public class SpeakerEventRepository : GenericRepositoryAsync<SpeakerEvent>, ISpeakerEventRepository
+	{
+		#region Fields
+		private readonly DbSet<SpeakerEvent> _speakerEvent;
+		#endregion
+		#region Constructors
+		public SpeakerEventRepository(AppDbContext dbContext) : base(dbContext)
+		{
+			_speakerEvent = dbContext.Set<SpeakerEvent>();
+		}
+
+
+		#endregion
+
+		#region Actions
+		public async Task<List<SpeakerEvent>> GetSpeakerEventListAsync()
+		{
+			return await _speakerEvent.ToListAsync();
+		}
+		#endregion
+	}
+}
