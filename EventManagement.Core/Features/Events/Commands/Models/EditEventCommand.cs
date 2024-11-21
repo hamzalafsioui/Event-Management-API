51534ca7a1b82578bdfbf3bce057daeed4882097
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-21 23:25:29 +0000

    Add SpeakerIds to EditEventCommand

--- EventManagement.Core/Features/Events/Commands/Models/EditEventCommand.cs
+++ EventManagement.Core/Features/Events/Commands/Models/EditEventCommand.cs
@@ -4,6 +4,6 @@ using MediatR;
 namespace EventManagement.Core.Features.Events.Commands.Models
 {
 	public record EditEventCommand(int Id, string Title, string? Description, string Location,
-		DateTime StartTime, DateTime EndTime, int CategoryId, int CreatorId, int Capacity) : IRequest<Response<string>>;
+		DateTime StartTime, DateTime EndTime, int CategoryId, int CreatorId, int Capacity,List<int> SpeakerIds) : IRequest<Response<string>>;
 
 }
