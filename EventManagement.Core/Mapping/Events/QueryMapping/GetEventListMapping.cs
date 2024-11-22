cec8bcac340aab2caff45f4f92babe555c665fc7
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-22 20:49:33 +0000

    Add Mapping to Speakers List

--- EventManagement.Core/Mapping/Events/QueryMapping/GetEventListMapping.cs
+++ EventManagement.Core/Mapping/Events/QueryMapping/GetEventListMapping.cs
@@ -8,8 +8,13 @@ namespace EventManagement.Core.Mapping.Events
 		private void GetEventListMapping()
 		{
 			CreateMap<Event, GetEventListResponse>()
+				.ForMember(x => x.SpeakersList, opt => opt.MapFrom(src => src.SpeakerEvents))
 				.ForMember(x => x.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
 				.ForMember(x => x.CreatedBy, opt => opt.MapFrom(src => src.Creator.UserName));
+
+			CreateMap<SpeakerEvent, SpeakerResponse>()
+				.ForMember(dest => dest.SpeakerName, opt => opt.MapFrom(src => $"{src.Speaker.User.FirstName} {src.Speaker.User.LastName}"))
+				.ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Speaker.Bio));
 
 		}
 	}
