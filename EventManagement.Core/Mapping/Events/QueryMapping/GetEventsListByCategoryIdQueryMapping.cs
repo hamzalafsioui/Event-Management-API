c563ac87fb94ca1efc098ed51e025c48fe49647c
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-22 20:57:07 +0000

    Fix Mapping to be Map Speakers

--- EventManagement.Core/Mapping/Events/QueryMapping/GetEventsListByCategoryIdQueryMapping.cs
+++ EventManagement.Core/Mapping/Events/QueryMapping/GetEventsListByCategoryIdQueryMapping.cs
@@ -8,11 +8,17 @@ namespace EventManagement.Core.Mapping.Events
 		private void GetEventsListByCategoryIdQueryMapping()
 		{
 			CreateMap<Event, GetEventsListByCategoryIdResponse>()
+				.ForMember(dest => dest.Speakers, opt => opt.MapFrom(src => src.SpeakerEvents))
 				.ForMember(x => x.Id, opt => opt.MapFrom(x => x.EventId))
 				.ForMember(x => x.Creator, opt => opt.MapFrom(x => x.Creator.UserName))
 				.ForMember(x => x.StartTime, opt => opt.MapFrom(x => x.StartTime))
 				.ForMember(x => x.EndTime, opt => opt.MapFrom(x => x.EndTime))
 				.ForMember(x => x.Name, opt => opt.MapFrom(x => x.Title));
+
+			CreateMap<SpeakerEvent, SpeakerResponse>()
+				.ForMember(dest => dest.SpeakerName, opt => opt.MapFrom(src => $"{src.Speaker.User.FirstName} {src.Speaker.User.LastName}"))
+				.ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.Speaker.Bio));
+
 		}
 	}
 }
