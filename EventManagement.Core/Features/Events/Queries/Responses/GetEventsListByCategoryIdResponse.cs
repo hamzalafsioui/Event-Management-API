95de40a86cdbc788b6bc752578a9e80b3de9d176
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-22 20:57:37 +0000

    Add Speakers List

---
+++ EventManagement.Core/Features/Events/Queries/Responses/GetEventsListByCategoryIdResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace EventManagement.Core.Features.Events.Queries.Response
+{
+	public class GetEventsListByCategoryIdResponse
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Location { get; set; }
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public string Creator { get; set; }
+		public List<SpeakerResponse> Speakers { get; set; }
+	}
+}
