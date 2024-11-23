e6cb6a7d185f98e0165d1117e624c2c8e8692b07
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-23 19:16:40 +0000

    Implement TooManyRequestsException

---
+++ EventManagement.Core/Exceptions/TooManyRequestsException.cs
@@ -0,0 +1,12 @@
+﻿namespace EventManagement.Core.Exceptions
+{
+	public class TooManyRequestsException : Exception
+	{
+		public TooManyRequestsException(string message = "Too many requests. Please try again later.")
+			: base(message)
+		{
+
+		}
+
+	}
+}
