2ddd9b0dc55c8948f954a39cc352da48090d352a
hamzalafsioui <hamzalafsioui@gmail.com>
2024-11-24 18:13:14 +0000

    Fix Claims Store

--- EventManagement.Data/Helper/Authorization/ClaimsStore.cs
+++ EventManagement.Data/Helper/Authorization/ClaimsStore.cs
@@ -6,10 +6,12 @@ namespace EventManagement.Data.Helper.Authorization
 	{
 		public static List<Claim> claims = new List<Claim>()
 		{
-			new Claim("Get Event","false"),
-			new Claim("Create Event","false"),
-			new Claim("Edit Event","false"),
-			new Claim("Delete Event","false"),
+		  // Claims for event actions with initial "false" state, indicating no permissions
+            new Claim("Create Event", "false"),
+			new Claim("Edit Event", "false"),
+			new Claim("Cancel Event", "false"),
+			new Claim("Delete Event", "false")
+
 		};
 	}
 }
