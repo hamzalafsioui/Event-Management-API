1c9ed4836e3f879e20dd42b316d1dee8ad2951eb
hamzalafsioui <hamzalafsioui@gmail.com>
2026-05-05 15:05:29 +0100

    Implement CacheService

---
+++ EventManagement.Service/Implementations/CacheService.cs
@@ -0,0 +1,54 @@
+using EventManagement.Service.Abstracts;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventManagement.Service.Implementations
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public CacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var cachedValue = await _cache.GetStringAsync(key);
+            if (string.IsNullOrEmpty(cachedValue))
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(cachedValue);
+        }
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+            if (expiration.HasValue)
+            {
+                options.AbsoluteExpirationRelativeToNow = expiration;
+            }
+            else
+            {
+                // Default expiration of 1 hour if not specified
+                options.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+            }
+
+            var serializedValue = JsonSerializer.Serialize(value);
+            await _cache.SetStringAsync(key, serializedValue, options);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+    }
+}
